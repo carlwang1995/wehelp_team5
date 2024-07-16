17e7357d239aa92ca38bc9ec322a98cb104fd73e
Wei-Yao Wang <z5n9800z@gmail.com>
2024-07-16 12:30:41 +0800

    Create weather api router.

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
 import rain_router
+import weather_router
 
 app = FastAPI()
 
@@ -14,3 +15,4 @@ async def main_page():
     return FileResponse("index.html")
 
 app.include_router(rain_router.router)
+app.include_router(weather_router.weather)
