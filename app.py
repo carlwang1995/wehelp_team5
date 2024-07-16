abc724b292c7143b4a9cbe9da99dc86bc4982815
moiv3 <159528029+moiv3@users.noreply.github.com>
2024-07-16 12:52:31 +0800

    modify app.py to mount static folder "icon"

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app = FastAPI()
 
 app.mount("/css", StaticFiles(directory="css"), name="static_css")
 app.mount("/js", StaticFiles(directory="js"), name="static_js")
+app.mount("/icon", StaticFiles(directory="icon"), name="static_icon")
 
 @app.get("/")
 async def main_page():
